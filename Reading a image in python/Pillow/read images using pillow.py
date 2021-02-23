7e16a1e8304f1bd7e9d130b2311f29c2bfc960c0
Abdullah-ICT-14 <mdabdullah01728@gmail.com>
2021-02-23 20:01:56 +0600

    Reading a image using pillow libraries

---
+++ Reading a image in python/Pillow/read images using pillow.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 23 19:36:21 2021
+
+@author: Md.Abdullah
+"""
+
+from PIL import Image
+import numpy as np
+# Opening the image
+img=Image.open('m.jpg') 
+
+#Print the type of image.Pillow image is not numpy array.
+print(type(img))
+
+# Showing the images
+img.show()
+
+#print the format of the image
+print(img.format)
+
+
+# Converting the img into numpy array using numpy asarray
+img1=np.asarray(img)
+print(img1)
+print(type(img1))
